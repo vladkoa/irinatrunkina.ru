dea3a7900ff9014ac4610f3a72d6dfc09c9be46d
vladkoa <zolo.vlad@gmail.com>
2026-09-02 10:57:15 +0800

    Configure Astro for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://vladkoa.github.io',
-  base: '/irinatrunkina.ru',
+  site: 'https://irinatrunkina.ru',
 });
